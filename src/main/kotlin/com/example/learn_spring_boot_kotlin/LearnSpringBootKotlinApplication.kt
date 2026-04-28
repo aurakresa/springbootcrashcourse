c942a973653029c591e822ddb8ead9a2050480bc
aurakresa <aurakresa12@gmail.com>
2026-04-28 21:13:09 +0700

    firsh commit

---
+++ src/main/kotlin/com/example/learn_spring_boot_kotlin/LearnSpringBootKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.learn_spring_boot_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearnSpringBootKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearnSpringBootKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/learn_spring_boot_kotlin/LearnSpringBootKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.learn_spring_boot_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearnSpringBootKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
