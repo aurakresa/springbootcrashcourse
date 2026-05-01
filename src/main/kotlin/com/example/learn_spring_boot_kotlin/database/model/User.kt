7d899cd4cd62608975180b727fbfa87f1e5d374a
aurakresa <aurakresa12@gmail.com>
2026-05-01 10:58:46 +0700

    feat: make user model collection

---
+++ src/main/kotlin/com/example/learn_spring_boot_kotlin/database/model/User.kt
@@ -0,0 +1,12 @@
+package com.example.learn_spring_boot_kotlin.database.model
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document(collection = "Users")
+data class User(
+    @Id val id: ObjectId = ObjectId.get(),
+    val hashedPassword: String,
+    val email: String
+)
