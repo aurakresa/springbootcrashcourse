d443dff86055c405f6eba0408f908e2e0d4a8ded
aurakresa <aurakresa12@gmail.com>
2026-05-06 21:06:42 +0700

    feat(auth): make hashEncorder for hashing password and matches for auth utility

---
+++ src/main/kotlin/com/example/learn_spring_boot_kotlin/security/HashEncorder.kt
@@ -0,0 +1,13 @@
+package com.example.learn_spring_boot_kotlin.security
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Component
+
+@Component
+class HashEncorder {
+    private val bcrypt = BCryptPasswordEncoder()
+
+    fun encode(raw: String): String? = bcrypt.encode(raw)
+
+    fun matches(raw: String, hashed: String) : Boolean = bcrypt.matches(raw, hashed)
+}
