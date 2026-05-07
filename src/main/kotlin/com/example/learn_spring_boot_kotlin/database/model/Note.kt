d1f5793575aa02dcc7f4d4e21e935f5d325643de
aurakresa <aurakresa12@gmail.com>
2026-05-07 12:42:17 +0700

    ref(Note Model): add title, createdAt, ownerId, and add annotaion @Id in id

--- src/main/kotlin/com/example/learn_spring_boot_kotlin/database/model/Note.kt
+++ src/main/kotlin/com/example/learn_spring_boot_kotlin/database/model/Note.kt
@@ -1,15 +1,16 @@
-package com.example.learn_spring_boot_kotlin.model
+package com.example.learn_spring_boot_kotlin.database.model
 
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.Instant
 
-@Document(collation = "Notes")
+@Document(collection = "Notes")
 data class Note(
-    @Id val id: ObjectId? = ObjectId.get(),
-    val tittle: String,
+    val title: String,
     val content: String,
     val color: Long,
-    val createdAt: Instant
+    val createdAt: Instant,
+    val ownerId: ObjectId,
+    @Id val id: ObjectId = ObjectId.get()
 )
